707593f55b85e6f0eaa1d342048a4ec11b53032f
Emmanuel Hernández <43358285+EmmanuelHdz01@users.noreply.github.com>
2024-07-16 20:54:03 -0600

    challenge

---
+++ challenge-alura-forohub/src/Monedas.java
@@ -0,0 +1,6 @@
+public record Monedas(Object conversion_rates,
+                      float USD,
+                      float ARS,
+                      float BRL,
+                      float COP) {
+}
